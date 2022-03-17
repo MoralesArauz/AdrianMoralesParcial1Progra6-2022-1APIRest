31dc08323d2b5bd9eaaed4388ac92c15aab30f7d
Adrian Morales <m-akuk@hotmail.com>
2022-03-16 20:28:04 -0600

    Add project files.

---
+++ AdrianMoralesParcial1API/Models/Activo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdrianMoralesParcial1API.Models
+{
+    public partial class Activo
+    {
+        public int Idactivo { get; set; }
+        public string NombreActivo { get; set; } = null!;
+        public string? NumeroSerie { get; set; }
+        public decimal CostoActivo { get; set; }
+        public string? Descripcion { get; set; }
+        public string Ubicacion { get; set; } = null!;
+        public string Responsable { get; set; } = null!;
+        public DateTime FechaRegistro { get; set; }
+        public bool? EstadoActivo { get; set; }
+    }
+}
---
+++ AdrianMoralesParcial1API/Startup.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdrianMoralesParcial1API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdrianMoralesParcial1API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+           
+
+
+
+            var conn = @"SERVER=.;DATABASE=P6Assets20221;Trusted_Connection=True;";
+
+            services.AddDbContext<P6Assets20221Context>(options => options.UseSqlServer(conn));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AdrianMoralesParcial1API", Version = "v1" });
+            });
+            
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AdrianMoralesParcial1API"));
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            //TODO: revisar si hace falta alguna config para uso de JWT
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
